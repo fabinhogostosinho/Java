fdd4097b754fc053a670384bf1800d3da6af0641
Fábio Henrique Santos <golfinho518@gmail.com>
2025-10-10 16:38:27 -0300

    update

--- CaixadeLoja/src/sistemaCaixa/system.java
+++ CaixadeLoja/src/sistemaCaixa/system.java
@@ -17,85 +17,69 @@ public class system {
 
         // Mostra produtos e estoque
         System.out.println("Produtos disponíveis:");
-        for (int i = 0; i < produtos.length; i++) {
-            System.out.println((i + 1) + " - " + produtos[i] + " (Estoque: " + estoque[i] + ")");
-        }
+        System.out.println("1 - " + produtos[0] + " (Estoque: " + estoque[0] + ")");
+        System.out.println("2 - " + produtos[1] + " (Estoque: " + estoque[1] + ")");
+        System.out.println("3 - " + produtos[2] + " (Estoque: " + estoque[2] + ")");
+        System.out.println("4 - " + produtos[3] + " (Estoque: " + estoque[3] + ")");
+        System.out.println("5 - " + produtos[4] + " (Estoque: " + estoque[4] + ")");
+        System.out.println("6 - " + produtos[5] + " (Estoque: " + estoque[5] + ")");
 
         // Escolha do produto
         System.out.print("\nEscolha o número do produto desejado: ");
         int escolha = sc.nextInt();
-        if (escolha < 1 || escolha > produtos.length) {
+        if (escolha < 1 || escolha > 6) {
             System.out.println("Opção inválida!");
-            return;
-        }
-
-        String produtoEscolhido = produtos[escolha - 1];
-        int qtdEstoque = estoque[escolha - 1];
-
-        // Quantidade desejada
-        System.out.print("Informe a quantidade desejada: ");
-        int quantidade = sc.nextInt();
-        if (quantidade <= 0) {
-            System.out.println("Valor incorreto, informe pelo menos um produto!");
-            return;
-        }
-        if (quantidade > qtdEstoque) {
-            System.out.println("Não temos essa quantidade desse produto, tente outra quantidade!");
-            return;
-        }
-
-        // Valor do produto
-        System.out.print("Informe o valor unitário do produto R$: ");
-        double valorUnitario = sc.nextDouble();
-
-        // Cálculo do total
-        double totalBruto = valorUnitario * quantidade;
-        double desconto = 0;
-
-        if (totalBruto > 200) {
-            desconto = 0.15;
-        } else if (totalBruto >= 100) {
-            desconto = 0.10;
         } else {
-            desconto = 0.05;
-        }
+            String produtoEscolhido = produtos[escolha - 1];
+            int qtdEstoque = estoque[escolha - 1];
 
-        double valorComDesconto = totalBruto - (totalBruto * desconto);
-
-        // Pergunta sobre parcelamento
-        System.out.print("Deseja parcelar? (S/N): ");
-        char resposta = sc.next().toUpperCase().charAt(0);
-
-        double valorParcela = 0;
-        int parcelas = 0;
-
-        if (resposta == 'S') {
-            System.out.print("Quantas parcelas (máximo 5)? ");
-            parcelas = sc.nextInt();
-
-            if (parcelas < 1 || parcelas > 5) {
-                System.out.println("Número de parcelas inválido! Será considerado pagamento à vista.");
+            // Quantidade desejada
+            System.out.print("Informe a quantidade desejada: ");
+            int quantidade = sc.nextInt();
+            if (quantidade <= 0) {
+                System.out.println("Valor incorreto, informe pelo menos um produto!");
+            } else if (quantidade > qtdEstoque) {
+                System.out.println("Não temos essa quantidade desse produto, tente outra quantidade!");
             } else {
-                valorParcela = valorComDesconto / parcelas;
+                // Valor do produto
+                System.out.print("Informe o valor unitário do produto R$: ");
+                double valorUnitario = sc.nextDouble();
+
+                // Cálculo do total
+                double totalBruto = valorUnitario * quantidade;
+                double desconto = 0;
+
+                if (totalBruto > 200) {
+                    desconto = 0.15;
+                } else if (totalBruto >= 100) {
+                    desconto = 0.10;
+                } else {
+                    desconto = 0.05;
+                }
+
+                double valorComDesconto = totalBruto - (totalBruto * desconto);
+
+                // Pergunta sobre parcelamento
+                System.out.print("Deseja parcelar? (S/N): ");
+                String resposta = sc.next();
+
+                // Pergunta sobre número de parcelas
+                System.out.print("Quantas parcelas (máximo 5)? ");
+                int parcelas = sc.nextInt();
+                double valorParcela = valorComDesconto / parcelas;
+
+                // Mensagem final
+                System.out.println("\n----- RESUMO DA COMPRA -----");
+                System.out.println("Cliente: " + nome);
+                System.out.println("Produto: " + produtoEscolhido);
+                System.out.println("Quantidade: " + quantidade);
+                System.out.println("Valor original: R$ " + totalBruto);
+                System.out.println("Desconto aplicado: " + (int)(desconto * 100) + "%");
+                System.out.println("Valor com desconto: R$ " + valorComDesconto);
+                System.out.println("Parcelado em " + parcelas + "x de R$ " + valorParcela);
+                System.out.println("-----------------------------");
+                System.out.println("Obrigado pela compra, " + nome + "! Volte sempre!");
             }
         }
-
-        // Mensagem final
-        System.out.println("\n----- RESUMO DA COMPRA -----");
-        System.out.println("Cliente: " + nome);
-        System.out.println("Produto: " + produtoEscolhido);
-        System.out.println("Quantidade: " + quantidade);
-        System.out.println("Valor original: R$ " + String.format("%.2f", totalBruto));
-        System.out.println("Desconto aplicado: " + (int)(desconto * 100) + "%");
-        System.out.println("Valor com desconto: R$ " + String.format("%.2f", valorComDesconto));
-
-        if (parcelas > 0 && parcelas <= 5) {
-            System.out.println("Parcelado em " + parcelas + "x de R$ " + String.format("%.2f", valorParcela));
-        } else {
-            System.out.println("Pagamento à vista.");
-        }
-
-        System.out.println("-----------------------------");
-        System.out.println("Obrigado pela compra, " + nome + "! Volte sempre!");
     }
 }
